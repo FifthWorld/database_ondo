7659306474e5abc1f13eb5d67eee7dbb98d4d289
unknown <LRB@LRB-HP.(none)>
2013-04-11 12:15:12 +0100

    database_ondo changes

--- extension/table_changes.sql
+++ extension/table_changes.sql
@@ -1,4 +1,14 @@
-﻿ALTER TABLE application.application
+﻿--Changes added on Monday, 04/08/2013 by Sam
+
+ALTER TABLE application.application_historic
 ADD document_verified boolean NOT NULL DEFAULT false;
+
+--Table changes to the party and party_historic table to support date of birth on Tuesday, 09 April 2013
+ALTER table party.party
+ADD date_of_birth date;
+
+ALTER TABLE party.party_historic
+ADD date_of_birth date;
+
 
 
