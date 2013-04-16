12b5c08606d59372df20cc5c8c002f535a9a354b
Deolu Akande <akandeadeoluwa@gmail.com>
2013-04-16 16:19:38 +0100

    Saving changes from previous edit

--- migration/sola_populate_shapefiles.sql
+++ migration/sola_populate_shapefiles.sql
@@ -9,3 +9,7 @@ INSERT INTO cadastre.spatial_unit (id, dimension_code, label, surface_relation_c
 
 
 --INSERT VALUES FOR Wards
+
+INSERT INTO cadastre.spatial_unit (id, dimension_code, label, surface_relation_code, geom, level_id, change_user) 
+	SELECT uuid_generate_v1(), '2D', name, 'onSurface', the_geom, (SELECT id FROM cadastre.level WHERE name='Wards') As l_id, 'test' AS ch_user 
+	FROM interim_data.wards WHERE (ST_GeometryN(the_geom, 1) IS NOT NULL);
