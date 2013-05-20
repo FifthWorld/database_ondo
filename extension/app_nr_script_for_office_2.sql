1069b17bd9c223cdf671d910ba1b84fc9cee4808
Deolu Akande <akandeadeoluwa@gmail.com>
2013-05-20 13:49:00 +0100

    Added the scripts for other offices into the extension directory

---
+++ extension/app_nr_script_for_office_2.sql
@@ -0,0 +1,14 @@
+DROP SEQUENCE IF EXISTS application.application_nr_seq;
+
+--application number sequence updated by Sam on 4/08/2013 at 1:28am-- it is expected to start from one and loop through to 99999
+--each office should have a different sequence range. Considering three offices now, the chosen convention would be as follows
+---office 1: 1-1,999,999
+---office 2: 2,000,000-3,999,999
+---office 3: 3,000,000-4,999,999
+---the following sequence would  be for office 1
+CREATE SEQUENCE application.application_nr_seq
+	INCREMENT 1
+	MINVALUE 2000000
+	MAXVALUE 2999999
+	START 2000000
+	CACHE 1;
