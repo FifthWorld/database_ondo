84b1b95ad9d406fc1620e06b3890602317501ccd
Mariapaola Rizzo <mariapaola.rizzo@gmail.com>
2013-07-05 10:13:07 +0200

    Added "Record Lien" as new Request Type  LH #4

--- extension/reference_data.sql
+++ extension/reference_data.sql
@@ -243,23 +243,23 @@ UPDATE source.spatial_source_type SET display_value = 'Survey Data', status = 'c
 
 --INSERT INTO spatial_source_type VALUES ('surveyData', 'Survey Data (Coordinates)::::Rilevamento Data', 'c', 'Extension to LADM');
 
+--
+--
+---------- Updates  KANO LH tickets #3,4,5, 16  (04/07/2013) ----
+--
+--
 
---
----------- Updates for LH tickets #3,16,5   (04/07/2013) ----
---
 --
 -- Data for Name: administrative_source_type; Type: TABLE DATA; Schema: source; Owner: postgres
 --
 UPDATE source.administrative_source_type SET display_value = 'Boundary Definition'  WHERE code = 'cadastralSurvey';
+insert into source.administrative_source_type(code, display_value, status) values('recordLien', 'Lien', 'c');
 
 --
 -- Data for Name: spatial_source_type; Type: TABLE DATA; Schema: source; Owner: postgres
 --
 
 UPDATE source.spatial_source_type SET display_value = 'Boundary Definition'  WHERE code = 'cadastralSurvey';
-
-
-
 
 --
 -- Data for Name: rrr_type; Type: TABLE DATA; Schema: administrative; Owner: postgres
@@ -268,13 +268,21 @@ UPDATE source.spatial_source_type SET display_value = 'Boundary Definition'  WHE
 UPDATE administrative.rrr_type SET display_value = 'Public Land'  WHERE code = 'stateOwnership';
 insert into administrative.rrr_type(code, rrr_group_type_code, display_value, is_primary, share_check, party_required, status) values('regnDeeds', 'responsibilities', 'Deed Registration', true, false, false, 'c');
 insert into administrative.rrr_type(code, rrr_group_type_code, display_value, is_primary, share_check, party_required, status) values('regnPowerOfAttorney', 'responsibilities', 'Power of Attorney', true, false, false, 'c');
+insert into administrative.rrr_type(code, rrr_group_type_code, display_value, is_primary, share_check, party_required, status) values('recordLien', 'restrictions', 'Lien', false, true, true, 'c');
 
 --
 -- Data for Name: request_type; Type: TABLE DATA; Schema: application; Owner: postgres
 --
 
-
 UPDATE application.request_type SET rrr_type_code = null, type_action_code = null  WHERE code = 'systematicRegn';
+insert into application.request_type(code, request_category_code, display_value, status, nr_days_to_complete, base_fee, area_base_fee, value_base_fee, nr_properties_required, notation_template, rrr_type_code, type_action_code) values('recordLien', 'registrationServices', 'Record Lien', 'c', 5, 5.00, 0.00, 0, 1, 'Lien to <lender>', 'recordLien', 'new');
+
+--
+-- Data for Name: request_type_requires_source_type; Type: TABLE DATA; Schema: application; Owner: postgres
+--
+
+insert into application.request_type_requires_source_type(source_type_code, request_type_code) values('recordLien', 'recordLien');
+insert into application.request_type_requires_source_type(source_type_code, request_type_code) values('title', 'recordLien');
 
 --
 -- Data for Name: language; Type: TABLE DATA; Schema: system; Owner: postgres
@@ -282,4 +290,20 @@ UPDATE application.request_type SET rrr_type_code = null, type_action_code = nul
 
 delete from system.language where code =  'it-IT' ;
 
+
+
+--- Added Role for record Lien LH # 4
+--
+-- Data for Name: approle;approle_appgroup Type: TABLE DATA; Schema: system; Owner: postgres
+--
+
+insert into system.approle(code, display_value, status, description) values('recordLien', 'Record Lien', 'c', 'Allows to make changes for registration of lien');
+
+
+--
+-- Data for Name: approle;approle_appgroup Type: TABLE DATA; Schema: system; Owner: postgres
+--
+
+INSERT INTO system.approle_appgroup (approle_code, appgroup_id) (SELECT 'recordLien', id FROM system.appgroup WHERE "name" = 'Land Deeds');  
+INSERT INTO system.approle_appgroup (approle_code, appgroup_id) values ('recordLien', 'super-group-id');  
 
